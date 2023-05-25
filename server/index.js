0b342c26901583d874726bf015aeacc9dcae0d6a
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-05-25 17:14:15 +0530

    deploy11

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(
     cors({
         credentials: true,
-        origin: "http://localhost:5173",
+        origin: "https://sutherland.onrender.com/",
     })
 );
 app.use("/auth", authRouter);
